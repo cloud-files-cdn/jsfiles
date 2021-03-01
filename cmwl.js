a42fb2eef55ed580b5e050d4ddee9e0d54f51d2a
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-03-01 20:01:19 +0800

    1

--- cmwl.js
+++ cmwl.js
@@ -19,13 +19,7 @@ $('#content').before('<div class="wr">'+
 '<a href="https://9996219.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337735.com/7996ea7b28e2482987289466ec3fcd25.gif"></a>'+
 '<a href="https://9996219.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337735.com/7996ea7b28e2482987289466ec3fcd25.gif"></a>'+
 '<a href="https://581733.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://jehudf.com/edb492d16c4a4a95bc058967d201a3c8.gif"></a>'+
-'<a href="https://x352.cc" rel="external nofollow" target="_blank"><img src="https://ae02.alicdn.com/kf/Ha5bf0be010f74aad886897a31ec8af66L.jpg"></a>'+
-'<a href="https://x352.cc" rel="external nofollow" target="_blank"><img src="https://ae02.alicdn.com/kf/Ha5bf0be010f74aad886897a31ec8af66L.jpg"></a>'+
-'<a href="https://x352.cc" rel="external nofollow" target="_blank"><img src="https://ae02.alicdn.com/kf/Ha5bf0be010f74aad886897a31ec8af66L.jpg"></a>'+
 '<a href="http://103.15.106.51:7777/66070592.html" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/xiaoxingxing9-1003.gif"></a>'+
-'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
-'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
-'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
 '<a href="https://6661791.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337751.com/95b85b41f67749b2a9749c94b2610512.gif"></a>'+
 '<a href="https://6661791.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337751.com/95b85b41f67749b2a9749c94b2610512.gif"></a>'+
 '<a href="https://7595527.com/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://3337738.com/109bec225ce34533a5a0a0f5a3816db2.gif"></a>'+
