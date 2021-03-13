9dcdbaa14d6044d8d8057752a7172bb2dc0a01c8
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-03-13 16:56:47 +0800

    1

--- lieqi.js
+++ lieqi.js
@@ -14,7 +14,7 @@ $('#tophf').prepend(''+
 '<a href="https://k4766.com/?register=1" rel="external nofollow" target="_blank"><img src="https://3336657.com/709c773feb154ca58b2e89a509086df2.gif"></a>'+
 //'<a href="http://6667020.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337735.com/7996ea7b28e2482987289466ec3fcd25.gif"></a>'+
 //'<a href="http://6667020.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337735.com/7996ea7b28e2482987289466ec3fcd25.gif"></a>'+
-//'<a href="https://6661791.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337751.com/95b85b41f67749b2a9749c94b2610512.gif"></a>'+
+'<a href="https://6661791.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337751.com/95b85b41f67749b2a9749c94b2610512.gif"></a>'+
 //'<a href="https://6661791.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://3337751.com/95b85b41f67749b2a9749c94b2610512.gif"></a>'+
 '');
 
