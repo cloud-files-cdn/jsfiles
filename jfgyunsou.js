d99b5f3d6ce23add39aa2dd06a2f23373beddf97
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-06-08 11:23:13 +0800

    1

--- jfgyunsou.js
+++ jfgyunsou.js
@@ -1,1 +1,1 @@
-$('.b-listtab-main').eq(0).after('<a style="margin-top:15px;" href="https://tyc85a.com/" target="_blank"><img src="https://www.hualigs.cn/image/609394e840a1e.jpg" style="width:100%;"  /></a>');
+//$('.b-listtab-main').eq(0).after('<a style="margin-top:15px;" href="https://tyc85a.com/" target="_blank"><img src="https://www.hualigs.cn/image/609394e840a1e.jpg" style="width:100%;"  /></a>');
