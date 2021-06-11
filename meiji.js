719eb21f847389f1afb4b80a7be26ac8a8ad5995
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-06-11 22:50:58 +0800

    1

--- meiji.js
+++ meiji.js
@@ -4,6 +4,7 @@ document.write('<style> #ad_top img{width:100%;} @media only screen and (min-wid
 function randomsort(a, b) {
     return Math.random()>.5 ? -1 : 1;
 }
+$('#ad_top').append('<a href="https://h8814.com?register=1" rel="external nofollow" target="_blank"><img src="https://ahwangtao.com/34f64f19ad9443acb79afe8e36f7fb04.gif"></a>');
 var arr = [{"link":"https://v5803.com?register=1","img":"https://lvchichecheng.com/e42e80e9879346aaa9af95707ff337ca.gif"},{"link":"https://t888996.com/?register=1","img":"https://700nmshnz.com/ee1c41777da0479f92faa6755973e7c4.gif"},{"link":"https://x28885.com/?register=1","img":"https://5551722.com/452094222bb8448596bf5b67d27a2a39.gif"},{"link":"https://k4766.com/?register=1","img":"https://3336657.com/709c773feb154ca58b2e89a509086df2.gif"},{"link":"https://v63399.com?register=1","img":"https://3337785.com/214402b0b3454f61ba25b5a0992b4b96.gif"}];
 //arr.sort(randomsort);
 for(key in arr)
