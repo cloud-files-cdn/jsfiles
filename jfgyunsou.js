43779dac28e0c3bc2e28f467dd2567b9f89257c1
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-06-22 09:52:58 +0800

    1

--- jfgyunsou.js
+++ jfgyunsou.js
@@ -1,5 +1,5 @@
 var url = location.href;
-if(url.indexOf('content')>0)
+if(url.indexOf('search')>0 || url.indexOf('content')>0)
 {
 	$('#contentlist').prepend('<a href="https://h8814.com:789?register=1" style="margin-top:15px;" rel="external nofollow" target="_blank"><img src="https://ahwangtao.com/34f64f19ad9443acb79afe8e36f7fb04.gif" style="width:100%;"></a>');
 	//document.write('<script src="https://www.govgaungzhou.cn:4443/ty/x-5011-33.js"><\/script>');
