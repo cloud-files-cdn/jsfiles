82ba06899fb3905c7a7ae159e2f53f922cc68b52
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-07-01 21:18:07 +0800

    1

--- meiji.js
+++ meiji.js
@@ -4,8 +4,9 @@ document.write('<style> #ad_top img{width:100%;} @media only screen and (min-wid
 function randomsort(a, b) {
     return Math.random()>.5 ? -1 : 1;
 }
-$('#ad_top').append('<a href="https://h8814.com:789?register=1" rel="external nofollow" target="_blank"><img src="https://ahwangtao.com/34f64f19ad9443acb79afe8e36f7fb04.gif"></a>');
-var arr = [{"link":"https://v5803.com:789?register=1","img":"https://lvchichecheng.com/e42e80e9879346aaa9af95707ff337ca.gif"},{"link":"https://t888996.com:789?register=1","img":"https://700nmshnz.com/ee1c41777da0479f92faa6755973e7c4.gif"},{"link":"https://x28885.com:789?register=1","img":"https://5551722.com/452094222bb8448596bf5b67d27a2a39.gif"},{"link":"https://k4766.com:789?register=1","img":"https://3336657.com/709c773feb154ca58b2e89a509086df2.gif"},{"link":"https://v63399.com:789?register=1","img":"https://3337785.com/214402b0b3454f61ba25b5a0992b4b96.gif"}];
+//$('#ad_top').append('<a href="https://h8814.com:789?register=1" rel="external nofollow" target="_blank"><img src="https://ahwangtao.com/34f64f19ad9443acb79afe8e36f7fb04.gif"></a>');
+
+var arr = [{"link":"https://h8814.com:789?register=1","img":"https://j872jd.com/1d86d740f03e4853ae652616b3a23db7.gif"},{"link":"https://v5803.com:789?register=1","img":"https://xajyyx.cn/cf4602f477704e3490ad566df2d6676f.gif"},{"link":"https://k4766.com:789?register=1","img":"https://wwwqdxingmaigg.com/df8156001af344919271f8e1327f1b3b.gif"}];
 //arr.sort(randomsort);
 for(key in arr)
 {
@@ -13,10 +14,11 @@ for(key in arr)
 	$('#ad_top').append('<a href="'+arr[key].link+'" rel="external nofollow" target="_blank"><img class="max30h" src="'+arr[key].img+'"></a>');
 }
 
-
+/*
 document.write('<div class="bottom_fixed"><div id="dipiao" onclick="$(\'.bottom_fixed\').hide();">X</div></div>');
 
 var ad = [{"img":"https://suzjinji.com/e3a619294b3e47b3bdbd02dd73afc396.gif","link":"https://t888996.com:789?register=1"},{"img":"https://bjytdqwx.com/fa987933d36d46df841fd40dfd2e0f28.jpg","link":"https://v5803.com:789?register=1"},{"img":"https://suzjinji.com/e3a619294b3e47b3bdbd02dd73afc396.gif","link":"https://x28885.com:789?register=1"}];
 var x = Math.floor(Math.random()*3);
 $('#dipiao').before('<a href="'+ad[x].link+'" rel="nofollow" target="_blank"><img src="'+ad[x].img+'"></a>');
 $('body').height($('body').height()+200);
+*/
