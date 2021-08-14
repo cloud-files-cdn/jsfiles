9611428530a3a5bd544370c8a6b1c428561d95e4
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-08-14 08:52:35 +0800

    1

--- content/91kcontent.js
+++ content/91kcontent.js
@@ -1,2 +1,2 @@
-$('.links').append('<a href="https://xn--y5qq4d96k0p6a.com" target="_blank">第一福利導航</a>  <a href="https://自拍图库.com/" target="_blank">自拍图库</a>  <a href="https://网红精选.com/" target="_blank">网红精选</a><a href="https://bbinnv.com/" target="_blank">权威博彩导航</a>');
+$('.links').append('<a href="https://xn--y5qq4d96k0p6a.com" target="_blank">第一福利導航</a>  <a href="https://自拍图库.com/" target="_blank">自拍图库</a><a href="https://集芳阁.com/" target="_blank">集芳阁</a><a href="https://bbinnv.com/" target="_blank">权威博彩导航</a>');
 $('.footer .container').before('<p>合作：jiuyaokanteam@outlook.com</p>');
