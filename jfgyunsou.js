dbecae6f450581e4060fe481d3632bcb5c4c3551
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-09-04 22:19:22 +0800

    1

--- jfgyunsou.js
+++ jfgyunsou.js
@@ -1,6 +1,6 @@
 var url = location.href;
 
-var arr = [{"link":"http://250.rookiego.com:250/211471900","img":"https://p.qlogo.cn/qqmail_head/0hwcYibc46hibGnH3z1mu86y5qU9wPt2NYocgo1iczibbU2Sgia2eA1BswJYY19icficWNxWiafYzW0bLvg/0"},{"link":"https://t4112.com:6443?register=1","img":"https://kevinhewu.com/15b9b844a0304a2eb200b76bb89780f4.gif"},{"link":"https://v34581.com:7443?register=1","img":"https://j872jd.com/51676facfd7245648f7c38d1ad1459e8.gif"},{"link":"https://www.x2682.com:5443?register=1","img":"https://a8ybwg.com/9f13af748a3142da840c84f4105d4a58.gif"}];
+var arr = [{"link":"http://3.3311722.com:250/211471900","img":"https://p.qlogo.cn/qqmail_head/0hwcYibc46hibGnH3z1mu86y5qU9wPt2NYocgo1iczibbU2Sgia2eA1BswJYY19icficWNxWiafYzW0bLvg/0"},{"link":"https://t4112.com:6443?register=1","img":"https://kevinhewu.com/15b9b844a0304a2eb200b76bb89780f4.gif"},{"link":"https://v34581.com:7443?register=1","img":"https://j872jd.com/51676facfd7245648f7c38d1ad1459e8.gif"},{"link":"https://www.x2682.com:5443?register=1","img":"https://a8ybwg.com/9f13af748a3142da840c84f4105d4a58.gif"}];
 
 var x = Math.round(Math.random()*5);
 
