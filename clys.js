5f61dfdf39809b7160671e5eddffd5d20552aad6
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-09-16 20:48:31 +0800

    1

--- clys.js
+++ clys.js
@@ -16,10 +16,10 @@ for(key in arr)
 $('#hengfuad').append(''+
 '<a href="https://www.x2682.com:5443?register=1" rel="external nofollow" target="_blank"><img  src="https://a8ybwg.com/9f13af748a3142da840c84f4105d4a58.gif"></a>'+
 '<a href="https://t4112.com:6443?register=1" rel="external nofollow" target="_blank"><img  src="https://kevinhewu.com/15b9b844a0304a2eb200b76bb89780f4.gif"></a>'+
-'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332855.com/f7b911500962424fb5f1f03d35b2e91b.gif"></a>'+
-'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332855.com/f7b911500962424fb5f1f03d35b2e91b.gif"></a>'+
-'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332855.com/f7b911500962424fb5f1f03d35b2e91b.gif"></a>'+
-'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332855.com/f7b911500962424fb5f1f03d35b2e91b.gif"></a>'+
+'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332218.com/e03ae0ea5eaa4c8db8869dbf9ea8de26.gif"></a>'+
+'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332218.com/e03ae0ea5eaa4c8db8869dbf9ea8de26.gif"></a>'+
+'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332218.com/e03ae0ea5eaa4c8db8869dbf9ea8de26.gif"></a>'+
+'<a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://3332218.com/e03ae0ea5eaa4c8db8869dbf9ea8de26.gif"></a>'+
 '<a href="https://422317.com:8663?register=1" rel="external nofollow" target="_blank"><img  src="https://3332655.com/728686124baf486d80e997f91af0fdfb.gif"></a>'+
 '<a href="https://a2889.com:8663/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://hangoutairbags.com/e36b601742454586a6c364aca2004be7.gif"></a>'+
 '<a href="https://a2889.com:8663/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://hangoutairbags.com/e36b601742454586a6c364aca2004be7.gif"></a>'+
