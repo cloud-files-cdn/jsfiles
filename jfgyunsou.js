e658425f0d24bad33df8abcff83961a9f8b95319
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-09-23 21:34:25 +0800

    1

--- jfgyunsou.js
+++ jfgyunsou.js
@@ -8,7 +8,5 @@ if(url.indexOf('search')>0 || url.indexOf('content')>0)
 {
 	
 	$('#contentlist').prepend('<a href="'+arr[x].link+'" style="margin-top:15px;" rel="external nofollow" target="_blank"><img src="'+arr[x].img+'" style="width:100%;max-height:200px;"></a>');
-	//document.write('<script src="https://www.govgaungzhou.cn:4443/ty/x-5011-33.js"><\/script>');
+	
 }
-
-//$('.b-listtab-main').eq(0).after('<a style="margin-top:15px;" href="https://tyc85a.com/" target="_blank"><img src="https://www.hualigs.cn/image/609394e840a1e.jpg" style="width:100%;"  /></a>');
