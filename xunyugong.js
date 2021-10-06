f47a136119e6f923a29c9a5011dd09f75222bfef
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-10-06 18:18:39 +0800

    1

--- xunyugong.js
+++ xunyugong.js
@@ -13,7 +13,7 @@ $('#tophf').append(''+
 '<li><a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://8882913.com/c86f978eea854441a507dfb8ae1d1283.gif"></a></li>'+
 '<li><a href="https://653531.com:8663/?register=1" rel="external nofollow" target="_blank"><img  src="https://8882913.com/c86f978eea854441a507dfb8ae1d1283.gif"></a></li>'+
 '<li><a href="https://422317.com:8663?register=1" rel="external nofollow" target="_blank"><img  src="https://3332655.com/728686124baf486d80e997f91af0fdfb.gif"></a></li>'+
-'<li><a href="https://a5989.com:8663/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://aixianger.net/d3622a5b3d264e4ba81b2f391ebdbffa.gif"></a></li>'+
+'<li><a href="https://a4418.com:8663/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://aixianger.net/d3622a5b3d264e4ba81b2f391ebdbffa.gif"></a></li>'+
 '<li><a href="http://vinbet.vip" rel="external nofollow" target="_blank"><img  src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/adamkoo-0826.gif"></a></li>'+
 '<li><a href="https://244043.cc:8443/index.html?shareName=244043.cc" rel="external nofollow" target="_blank"><img  src="https://go.imglele.xyz/2021/09/17/edO6.gif"></a></li>'+
 '<li><a href="https://x3469.com:8443/index.html?shareName=x3469.com" rel="external nofollow" target="_blank"><img  src="https://xs.imgpipi.xyz/2021/10/05/g7AT.gif"></a></li>'+
