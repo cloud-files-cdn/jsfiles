14b8f15196ac2726ef655b35cac7e189b80c12d6
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2021-12-15 16:04:35 +0800

    1

--- cjg.js
+++ cjg.js
@@ -35,6 +35,7 @@ $('#ad_top_cjg').append('<div class="topwz"><span>站长推荐：</span>'+
 '<a href="https://slf546.com?_c=tslf306" rel="external nofollow" target="_blank">涩里番成漫APP</a>'+
 '<a href="https://wjy4511.cn/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>'+
 '<a href="https://migukedown.tv/?channelCode=86" target="_blank">咪咕视频APP</a>'+
+'<a href="https://ch1eujah.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>'+
 '</div>');
 
 
