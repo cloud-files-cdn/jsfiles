b555a16225686db8100df7b7ccaef925b4bb9fb0
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2022-01-17 11:10:18 +0800

    1

--- hansan.js
+++ hansan.js
@@ -30,8 +30,8 @@ $('#topview').append('<div class="topwz"><span>站长推荐：</span>'+
 '<a href="'+arr[x]+'" rel="external nofollow" target="_blank">抖阴短视频APP</a>'+
 '<a href="https://qqhjo.com/?dc=sqt00071" rel="external nofollow" target="_blank">麻豆传媒APP</a>'+
 '<a href="https://slfl30el.com?_c=tslf306" rel="external nofollow" target="_blank">涩里番成漫APP</a>'+
+'<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩APP</a>'+
 '<a href="https://wjy4511.cn/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>'+
 '<a href="https://chwjps44.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>'+
 '<a href="https://91sar2np.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>'+
-'<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩APP</a>'+
 '</div>');
