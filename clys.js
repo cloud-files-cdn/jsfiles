0542e4756757d49a7ea981c8b816623d0a9be810
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2022-01-29 07:31:15 +0800

    1

--- clys.js
+++ clys.js
@@ -31,7 +31,7 @@ $('#hengfuad').append('<div class="topwz"><span>站长推荐：</span>'+
 '<a href="https://slfaz0c6.com?_c=tslf306" rel="external nofollow" target="_blank">涩里番成漫APP</a>'+
 '<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩APP</a>'+
 '<a href="https://wjy4511.cn/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>'+
-'<a href="https://ch9bmcwk.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>'+
-'<a href="https://91w3ecaf.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>'+
+'<a href="https://chvkxkzu.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>'+
+'<a href="https://917dhzjo.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>'+
 '<a href="https://楼凤阁.com/?r=clys" target="_blank">楼凤阁小姐信息</a>'+
 '</div>');
