5773251b1a906e01c212500b8e3ccc3625055631
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2022-01-30 20:00:17 +0800

    1

--- cjg.js
+++ cjg.js
@@ -30,8 +30,8 @@ var x = Math.round(Math.random()*arr.length);
 $('#ad_top_cjg').append('<div class="topwz"><span>站长推荐：</span>'+
 '<a href="'+arr[x]+'" rel="external nofollow" target="_blank">抖阴短视频APP</a>'+
 '<a href="https://qqhjo.com/?dc=sqt00071" rel="external nofollow" target="_blank">麻豆传媒APP</a>'+
-'<a href="https://slfaz0c6.com?_c=tslf306" rel="external nofollow" target="_blank">涩里番成漫APP</a>'+
 '<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩APP</a>'+
+'<a href="https://slfaz0c6.com?_c=tslf306" rel="external nofollow" target="_blank">涩里番成漫APP</a>'+
 '<a href="https://wjy4511.cn/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>'+
 '<a href="https://chvkxkzu.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>'+
 '<a href="https://917dhzjo.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>'+
