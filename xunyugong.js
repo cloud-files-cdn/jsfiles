68550f385ff92fd48c841765b96efb92a2095e95
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2022-03-05 16:38:52 +0800

    1

--- xunyugong.js
+++ xunyugong.js
@@ -34,11 +34,11 @@ function shuffle(arr){
 }
 
 var arrapp = ['<a href="https://qqhjo.com/?dc=sqt00071" target="_blank">麻豆传媒APP</a>',
-  '<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩APP</a>',
-  '<a href="https://35z.cc/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>',
-  '<a href="https://ch2m1moj.com/?_c=offl1mh" target="_blank">51成人漫画APP</a>',
-  '<a href="https://91gi2ce0.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>',
-  '<a href="https://楼凤阁.com/?r=xunyugong" target="_blank">楼凤阁小姐信息</a>'];
+				  '<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩视频APP</a>',
+				  '<a href="https://35z.cc/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>',
+				  '<a href="https://ch2m1moj.com/?_c=offl1mh" target="_blank">51成漫APP</a>',
+				  '<a href="https://zhuren7.top/?channelCode=dsxk_5" target="_blank">大神成人直播APP</a>',
+				  '<a href="https://楼凤阁.com/" target="_blank">楼凤阁找小姐</a>'];
 var wzhtml='<div class="topwz"><span>站长推荐：</span>';
 $.each(shuffle(arrapp),function(index,value){
      wzhtml+=value;
