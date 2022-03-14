68c930210a1b4b91e6d7754b6a3a5e5a1c0b7e42
cloud-files-cdn <77315211+cloud-files-cdn@users.noreply.github.com>
2022-03-14 21:48:56 +0800

    1

--- hansan.js
+++ hansan.js
@@ -39,8 +39,8 @@ function shuffle(arr){
 var arrapp = ['<a href="https://qqhjo.com/?dc=sqt00071" target="_blank">麻豆传媒APP</a>',
 				  '<a href="https://7lil9i.com/?dc=bsqt34" target="_blank">逼哩逼哩视频APP</a>',
 				  '<a href="https://35z.cc/?channelCode=rrdyfl" target="_blank">软软成人直播APP</a>',
-				  '<a href="https://ch2m1moj.com/?_c=offl1mh" target="_blank">51成漫APP</a>',
-				  '<a href="https://91gi2ce0.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>',
+				  '<a href="https://chwu4hsj.com/?_c=offl1mh" target="_blank">51成漫APP</a>',
+				  '<a href="https://91yvf1zr.com/?ch=offl2cy" target="_blank">91次元成漫APP</a>',
 				  '<a href="https://luoyanglu.com/?TT9" target="_blank">陌陌视频APP</a>',
 				  '<a href="https://zhuren7.top/?channelCode=dsxk_5" target="_blank">大神成人直播APP</a>',
 				  '<a href="https://楼凤阁.com/" target="_blank">楼凤阁找小姐</a>'];
